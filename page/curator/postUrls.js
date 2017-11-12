64beda7f50cfcdab606a45be46d17b580b805fb2
kreid8 <kreid8@kent.edu>
2017-11-12 14:18:08 -0500

    added urls

--- page/curator/postUrls.js
+++ page/curator/postUrls.js
@@ -1,3 +1,4 @@
-var add-curator-url = "https://kent.greentbc.com/print_manager/printmanager/";
-var remove-curator-url = "";
+var add-curator-url = "https://kent.greentbc.com:8003/printmanager/AddNewCurator";
+var remove-curator-url = "https://kent.greentbc.com:8003/printmanager/RemoveCuratorFromDatabase";
 var edit-curator-url = "";
+var get-curator-url = "https://kent.greentbc.com:8003/printmanager/SelectAllFromTable?tablename=CURATORS"
--- page/resource/postUrls.js
+++ page/resource/postUrls.js
@@ -1,3 +1,3 @@
-var add-resource-url = "https://kent.greentbc.com/print_manager/printmanager/AddNewResource";
-var remove-resource-url = "";
+var add-resource-url = "https://kent.greentbc.com:8003/printmanager/AddNewResource";
+var remove-resource-url = "https://kent.greentbc.com:8003/printmanager/DeleteResource";
 var edit-resource-url = "";
